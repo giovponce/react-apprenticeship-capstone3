8b61b886d056065eca06d64402fb60b23ed167b4
Giovanna Ponce Castillo <giovanna.ponce@Giovannas-MacBook-Pro.local>
2022-01-27 20:37:27 -0600

    firestore integration, create/red/update/delete

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { StyledNoteContainer } from '../Utils/Styled Components/StyledContainer';
 import { StyledDescription, StyledTitle } from '../Utils/Styled Components/StyledText';
+import { updateDoc, deleteDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase-config';
 
-export default function Note() {
+
+
+export default function Note({note}) {
+
+    const updateNote = async (id, title, content) => {
+        const noteDoc = doc(db, "notes", id);
+        const newNote = {
+            title: title + 'modified from app',
+            content: content + 'modified from app'
+        }
+        await updateDoc(noteDoc, newNote)
+    }
+
+    const deleteNote = async (id) => {
+        const noteDoc = doc(db, "notes", id);
+        await deleteDoc(noteDoc);
+    }
+
     return (
         <StyledNoteContainer>
-            <StyledTitle>Title</StyledTitle>
-            <StyledDescription>Description</StyledDescription>
+            <StyledTitle>{note?.title}</StyledTitle>
+            <StyledDescription>{note?.content}</StyledDescription>
+            <button onClick={()=>{updateNote(note.id, note.title, note.content)}}>Edit</button>
+            <button onClick={()=>{deleteNote(note.id)}}>Delete</button>
         </StyledNoteContainer>
     )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Note from '../Components/Note';
 import { StyledH1 } from '../Utils/Styled Components/StyledText';
-
+import { db } from '../firebase-config';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 
 export default function Home() {
+
+  const [notes, setNotes] = useState([]);
+  const notesCollectionRef = collection(db, 'notes');
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const addNote = async () => {
+    await addDoc(notesCollectionRef, {
+      title: title,
+      content : content
+    });
+    setTitle('');
+    setContent('');
+  }
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      const data = await getDocs(notesCollectionRef);
+      setNotes(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+    }
+
+    fetchNotes();
+  });
+
   return (
     <div>
       <StyledH1>Home</StyledH1>
-      <Note/>
+      <input onChange={(event)=>{setTitle(event.target.value)}} type="text" placeholder="title" />
+      <input onChange={(event)=>{setContent(event.target.value)}} type="text" placeholder="content" />
+      <button onClick={addNote}>Add note</button>
+
+      {notes.map((note) => {
+        return (
+          <Note key={note.id} note={note} />
+        )
+      })}
     </div>
   )
 }
